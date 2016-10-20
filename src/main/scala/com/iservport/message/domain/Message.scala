6a6c5c3c510def9d6fcfa30deb96e34b5925a671
Mauricio Fernandes de Castro <mauricio@iservport.com>
2016-10-20 19:14:45 -0200

    1.2.1 Release
    Refactored message constructor.

--- src/main/scala/com/iservport/message/domain/Message.scala
+++ src/main/scala/com/iservport/message/domain/Message.scala
@@ -12,52 +12,22 @@ import scala.beans.BeanProperty
   * @param servicePath
   * @param config
   * @param defaults
+  * @param id
+  * @param template
+  * @param attachments
   */
 case class Message
 ( @BeanProperty sender: ContactData
 , @BeanProperty recipient: ContactData
 , @BeanProperty servicePath: String
 , @BeanProperty config: MessageConfig = MessageConfig()
-, @BeanProperty defaults: MessageDefaults = MessageDefaults()) {
-
-  @BeanProperty val template = "4853b17b-e5f2-4b8f-b313-7067088fb3c5"
-
-  @BeanProperty val attachments = new java.util.ArrayList[MessageAttachment]()
+, @BeanProperty defaults: MessageDefaults = MessageDefaults()
+, @BeanProperty id: String = UUID.randomUUID().toString.replaceAll("-", "")
+, @BeanProperty template: String = "4853b17b-e5f2-4b8f-b313-7067088fb3c5"
+, @BeanProperty attachments: java.util.List[MessageAttachment] = new java.util.ArrayList[MessageAttachment]()
+) {
 
   def add(attachment: MessageAttachment) = attachments.add(attachment)
-
-  val id = UUID.randomUUID().toString.replaceAll("-", "")
-
-  val getSenderEmail = sender.contactEmail
-
-  val getSenderName = sender.contactName
-
-  val headers: Map[String, String] = Map("template_id" -> template
-    , "${senderEmail}" -> sender.contactEmail
-    , "${senderName}" -> sender.contactName
-    , "${senderImageUrl}" -> sender.imageUrl
-    , "${recipientEmail}" -> recipient.contactEmail
-    , "${title}" -> config.title
-    , "${recipientFirstName}" -> recipient.contactName
-    , "${entityName}" -> recipient.entityName
-    , "${greeting}" -> defaults.greeting
-    , "${procedure}" -> config.procedure
-    , "${callToAction}" -> config.callToAction
-    , "${fallBack}" -> config.fallBack
-    , "${trailingInfo}" -> config.trailingInfo
-    , "${callBackUri}" -> s"${defaults.apiHome}${servicePath}/${id}"
-    , "${seeOnline}" -> defaults.seeOnline
-    , "${sentByText}" -> defaults.sentByText
-    , "${senderAddress}" -> sender.address
-    , "${disclaimer}" -> defaults.disclaimer
-    , "${unsubscribeText}" -> defaults.unsubscribeText
-    , "${unsubscribeCaption}" -> defaults.unsubscribeCaption
-    , "${unsubscribeUri}" -> s"${defaults.apiHome}/${defaults.unsubscribeService}"
-    , "${ensure}" -> defaults.ensure
-    , "${copyright}" -> defaults.copyright
-  ).filter(_._2.nonEmpty)
-
-  val getBody = s"${defaults.apiHome}/redirect/${template}/${id}"
 
 }
 
