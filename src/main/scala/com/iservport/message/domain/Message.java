c4f789e7c76f3ca019e39272e23a46c7834bae0e
Mauricio Fernandes de Castro <mauricio@iservport.com>
2016-11-26 08:23:52 -0200

    1.3.1 Release
    Added message token.

--- src/main/scala/com/iservport/message/domain/Message.java
+++ src/main/scala/com/iservport/message/domain/Message.java
@@ -16,6 +16,7 @@ public class Message {
     private MessageData messageData = new MessageData();
     private MessageDefaults defaults = new MessageDefaults();
     private String id = UUID.randomUUID().toString().replaceAll("-", "");
+    private String token = "";
     private String template = "4853b17b-e5f2-4b8f-b313-7067088fb3c5";
 
 }
